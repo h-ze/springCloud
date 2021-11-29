2bcf1ead5cbd183f7ab7c4a72b51757fa41815cc
heze <hz15858@163.com>
2021-11-29 17:25:53 +0800

    add 动态刷新配置

--- cloud-config-center3344/src/main/java/com/configcenter/ConfigCenterApplication.java
+++ cloud-config-center3344/src/main/java/com/configcenter/ConfigCenterApplication.java
@@ -6,6 +6,11 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+
+//访问地址 http://localhost:3344/main/config-dev.yml
+
+//全局通知 http://localhost:3344/actuator/bus-refresh
+//局部通知 http://localhost:3344/actuator/bus-refresh/config-client:3355
 public class ConfigCenterApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigCenterApplication.class,args);
