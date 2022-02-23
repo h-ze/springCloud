3e30f2139b7daff7c7e3808dc51d76f045ed2362
heze <hz15858@163.com>
2022-02-23 17:55:21 +0800

    update

--- cloud-server6001/src/main/java/com/hz/aliyun/OSSClientConstants.java
+++ cloud-server6001/src/main/java/com/hz/aliyun/OSSClientConstants.java
@@ -9,16 +9,16 @@ package com.hz.aliyun;
 public class OSSClientConstants {
     //阿里云API的外网域名
     //public static final String ENDPOINT = "oss-cn-shanghai.aliyuncs.com";
-    public static final String ENDPOINT = "oss-cn-beijing.aliyuncs.com";
+    public static final String ENDPOINT = "";
     //阿里云API的密钥Access Key ID
 
-    public static final String ACCESS_KEY_ID = "LTAI5tDfnTsZCoBdpwHB1b2o";
+    public static final String ACCESS_KEY_ID = "";
     //阿里云API的密钥Access Key Secret
-    public static final String ACCESS_KEY_SECRET = "6S96XJelwRUhDnQhoMmVDFFLZLYrmq";
+    public static final String ACCESS_KEY_SECRET = "";
     //阿里云API的bucket名称
-    public static final String BACKET_NAME = "oss-heze";
+    public static final String BACKET_NAME = "";
     //阿里云API的文件夹名称
-    public static final String FOLDER="test/";
+    public static final String FOLDER="";
 
 
 }
