8b4df374ddbcd083dcc1e0df089952db68627b1b
heze <hz15858@163.com>
2022-02-23 08:54:56 +0800

    update

--- cloud-server6001/src/main/java/com/hz/aliyun/OSSClientConstants.java
+++ cloud-server6001/src/main/java/com/hz/aliyun/OSSClientConstants.java
@@ -12,9 +12,9 @@ public class OSSClientConstants {
     public static final String ENDPOINT = "oss-cn-beijing.aliyuncs.com";
     //阿里云API的密钥Access Key ID
 
-    public static final String ACCESS_KEY_ID = "LTAI5tDfnTsZCoBdpwHB1b2o";
+    public static final String ACCESS_KEY_ID = "";
     //阿里云API的密钥Access Key Secret
-    public static final String ACCESS_KEY_SECRET = "6S96XJelwRUhDnQhoMmVDFFLZLYrmq";
+    public static final String ACCESS_KEY_SECRET = "git ";
     //阿里云API的bucket名称
     public static final String BACKET_NAME = "oss-heze";
     //阿里云API的文件夹名称
